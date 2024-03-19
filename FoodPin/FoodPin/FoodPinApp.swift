b2a342343516917ff4c8648eecf95c9f1335be37
SeckMohameth <mohamethseck34@gmail.com>
2024-03-18 22:40:46 -0400

    working with list

---
+++ FoodPin/FoodPin/FoodPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPinApp.swift
+//  FoodPin
+//
+//  Created by Mohameth Seck on 3/17/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RestaurantListView()
+        }
+    }
+}
---
+++ FoodPin/FoodPin/RestaurantListView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  FoodPin
+//
+//  Created by Mohameth Seck on 3/17/24.
+//
+
+import SwiftUI
+
+struct RestaurantListView: View {
+    
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    
+    var restaurantImages = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    
+    var body: some View {
+        List{
+            ForEach(restaurantNames.indices, id:\ .self) { index in
+                HStack(alignment: .top, spacing: 20) {
+                    Image(restaurantImages[index])
+                        .resizable()
+                        .frame(width: 120, height: 118)
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                               
+                    VStack(alignment: .leading) {
+                        Text("\(restaurantNames[index])")
+                            .font(.system(.title2, design: .rounded))
+                        
+                        Text("Type")
+                            .font(.system(.body, design: .rounded))
+                        
+                        Text("Location")
+                            .font(.system(.subheadline, design: .rounded))
+                    }
+                }
+                
+            }
+        }
+        .listStyle(.plain)
+    }
+}
+
+#Preview {
+    RestaurantListView()
+}
