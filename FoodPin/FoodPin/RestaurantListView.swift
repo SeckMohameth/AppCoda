0fe0b37cdcc8e133fd9a20620aa68bdfac46aec5
SeckMohameth <mohamethseck34@gmail.com>
2024-03-22 23:12:55 -0400

    continuing the list

--- FoodPin/FoodPin/RestaurantListView.swift
+++ FoodPin/FoodPin/RestaurantListView.swift
@@ -13,6 +13,8 @@ struct RestaurantListView: View {
     
     var restaurantImages = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
     
+    
+    
     var body: some View {
         List{
             ForEach(restaurantNames.indices, id:\ .self) { index in
