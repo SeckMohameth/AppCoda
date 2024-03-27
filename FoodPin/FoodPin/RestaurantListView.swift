fc5806b598f97b65917fabec29487f76fe55eb62
SeckMohameth <mohamethseck34@gmail.com>
2024-03-26 23:31:06 -0400

    extracted the image row text

--- FoodPin/FoodPin/RestaurantListView.swift
+++ FoodPin/FoodPin/RestaurantListView.swift
@@ -13,35 +13,52 @@ struct RestaurantListView: View {
     
     var restaurantImages = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
     
+    var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
+
+    var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
+    
     
     
     var body: some View {
         List{
             ForEach(restaurantNames.indices, id:\ .self) { index in
-                HStack(alignment: .top, spacing: 20) {
-                    Image(restaurantImages[index])
-                        .resizable()
-                        .frame(width: 120, height: 118)
-                        .clipShape(RoundedRectangle(cornerRadius: 20))
-                               
-                    VStack(alignment: .leading) {
-                        Text("\(restaurantNames[index])")
-                            .font(.system(.title2, design: .rounded))
-                        
-                        Text("Type")
-                            .font(.system(.body, design: .rounded))
-                        
-                        Text("Location")
-                            .font(.system(.subheadline, design: .rounded))
-                    }
-                }
+                BasicTextImageRow()
                 
             }
+            .listRowSeparator(.hidden)
         }
         .listStyle(.plain)
     }
 }
 
-#Preview {
+#Preview("Pin screen") {
     RestaurantListView()
+        .preferredColorScheme(.dark)
+}
+
+struct BasicTextImageRow: View {
+    var imageName: String
+    var name: String
+    var type: String
+    var location: String
+    
+    var body: some View {
+        HStack(alignment: .top, spacing: 20) {
+            Image(imageName)
+                .resizable()
+                .frame(width: 120, height: 118)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+            
+            VStack(alignment: .leading) {
+                Text(name)
+                    .font(.system(.title2, design: .rounded))
+                
+                Text(type)
+                    .font(.system(.body, design: .rounded))
+                
+                Text(location)
+                    .font(.system(.subheadline, design: .rounded))
+            }
+        }
+    }
 }
